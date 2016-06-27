73179315049615cabd3ec164dc46919fe78c9548
brainTrain <abductee@gmail.com>
2016-06-26 22:21:41 -0700

    add base url templating for index.html assets
    * not working in css might just do cdnizer which will be an improvement anyway

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -3,6 +3,8 @@
 import gulp from 'gulp';
 import config from '../config';
 import revReplace from 'gulp-rev-replace';
+import buffer from 'vinyl-buffer';
+import template from 'gulp-template';
 
 // Views task
 gulp.task('views', () => {
@@ -11,7 +13,11 @@ gulp.task('views', () => {
     ]);
 
     return gulp.src(config.views.src)
+        .pipe(template({baseUrl: config.views.devBaseUrl}))
         .pipe(gulp.dest(config.views.devDest))
+        .pipe(buffer())
+        .pipe(gulp.src(config.views.src))
         .pipe(revReplace({manifest: manifest}))
+        .pipe(template({baseUrl:  config.views.prodBaseUrl}))
         .pipe(gulp.dest(config.views.prodDest));
 });
