55a79dc938a6e12887a526e4f9c5d2b25562da99
brainTrain <abductee@gmail.com>
2016-10-01 08:46:12 -0700

    some gulp task updates
    * create a simple local process which runs build watch and connect
    * create a simple clean process which deletes /public and /dev
    * removed susy since I'm not using it
    * update gulp watch to listen to all sources and run the build task
    * update the build task to run the clean task and run the remainder after clean is done

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,10 +2,15 @@
 
 import gulp from 'gulp';
 import config from '../config';
+import runSequence from 'run-sequence';
 
 gulp.task('watch', () => {
-    gulp.watch(config.styles.watch, ['styles']);
-    gulp.watch(config.scripts.watch, ['scripts']);
-    gulp.watch(config.views.src, ['views']);
-    gulp.watch(config.images.src, ['images']);
+    let buildSources = [
+        config.styles.watch,
+        config.scripts.watch,
+        config.views.src,
+        config.images.src,
+        config.svg.src
+    ];
+    gulp.watch(buildSources, ['build']);
 });
