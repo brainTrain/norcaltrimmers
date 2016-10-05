dc3745961de91cab1d2f368e080aafb06622e20a
brainTrain <abductee@gmail.com>
2016-10-04 20:41:09 -0700

    add callback for error handling

--- app/media/js/safari-required-shim.js
+++ app/media/js/safari-required-shim.js
@@ -1,15 +1,13 @@
 // lovingly lifted from: http://www.html5rocks.com/en/tutorials/forms/constraintvalidation/#toc-safari
 
-function safariRequiredShim() {
+function safariRequiredShim(callback) {
     let forms = document.getElementsByTagName('form');
     for (let i = 0; i < forms.length; i++) {
-        forms[i].noValidate = true;
-
         forms[i].addEventListener('submit', function(event) {
             //Prevent submission if checkValidity on the form returns false.
             if (!event.target.checkValidity()) {
                 event.preventDefault();
-                //Implement you own means of displaying error messages to the user here.
+                callback();
             }
         }, false);
     }
