81c977ae60d8a94fd8d67a9878660211f984dadd
brainTrain <abductee@gmail.com>
2018-06-24 20:44:16 -0700

    adding a bunch of svg icons to the project
    * animate/add only one to index.html for now (not sure I wanna animate the others)

---
+++ app/media/js/flower-animations.js
@@ -0,0 +1,22 @@
+import anime from 'animejs';
+
+function flowerAnimations () {
+    const plantSvg = document.querySelector('#plant svg');
+    const plantPath = document.querySelector('#plant path');
+    console.log('plantPath', plantPath)
+
+    anime({
+        targets: plantSvg,
+        duration: 20000,
+        easing: 'easeInOutBack',
+        loop: true,
+        fill: [
+          '#87AB7B',
+          '#4F6242',
+          '#9B6679',
+          '#87AB7B'
+        ],
+    });
+}
+
+module.exports = flowerAnimations;
